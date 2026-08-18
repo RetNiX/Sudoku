817075cd7bf2ddb247b3b40565caa3dbd15552ee
RetNiX <kochick16@gmail.com>
2026-08-18 09:13:10 +0300

    Add number validation function

--- sudoku.c
+++ sudoku.c
@@ -11,4 +11,16 @@ bool is_valid(int curr_board[9][9], int checked_number, int row, int col) {
             return false;
         }
     }
+    // Checking if number exits in box 3x3
+    // Calculating the top-left corner position from the current row-col
+    int start_row = (row / 3) * 3;
+    int start_col = (col / 3) * 3;
+    for (int i = start_row; i < start_row + 3; i++) {
+        for(int j = start_col; j < start_col + 3; j++) {
+            if (curr_board[i][j] == checked_number) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
